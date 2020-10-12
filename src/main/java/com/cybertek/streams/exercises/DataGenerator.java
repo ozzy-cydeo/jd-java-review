fe346d2aaa3bd3ecb8ac280146defcfd0a261888
ozzy <ozzy@cybertekschool.com>
2020-10-12 13:28:39 -0400

    Java-Review

--- src/main/java/com/cybertek/streams/exercises/DataGenerator.java
+++ src/main/java/com/cybertek/streams/exercises/DataGenerator.java
@@ -59,6 +59,28 @@ public class DataGenerator {
             "Evans",cfoRole,Gender.MALE);
     public static User user5 = new User(1,LocalDateTime.now(),1,LocalDateTime.now().plusHours(1),1,"Barbara",
             "Martin",cmoRole,Gender.FEMALE);
+    public static User user6 = new User(6,LocalDateTime.now(),1,LocalDateTime.now().plusHours(1),1,"Tracy",
+            "Wilson",managerRole,Gender.FEMALE);
+    public static User user7 = new User(7, LocalDateTime.now(), 1, LocalDateTime.now().plusHours(1), 1, "Shaun",
+            "Hayns",employeeRole, Gender.MALE);
+    public static User user8 = new User(8, LocalDateTime.now(), 1, LocalDateTime.now().plusHours(1), 1, "Mike",
+            "Lugan",employeeRole, Gender.MALE);
+    public static User user9 = new User(9, LocalDateTime.now(), 1, LocalDateTime.now().plusHours(1), 1, "Daniel",
+            "Smith",employeeRole, Gender.MALE);
+    public static User user10 = new User(10, LocalDateTime.now(), 1, LocalDateTime.now().plusHours(1), 1, "Ellie",
+            "Smith",employeeRole, Gender.FEMALE);
+    public static User user11 = new User(11, LocalDateTime.now(), 1, LocalDateTime.now().plusHours(1), 1, "Emma",
+            "Jones",employeeRole, Gender.FEMALE);
+    public static User user12 = new User(12,LocalDateTime.now(),1,LocalDateTime.now().plusHours(1),1,"Sarah",
+            "Brown",supervisorRole,Gender.FEMALE);
+    public static User user13 = new User(1,LocalDateTime.now(),1,LocalDateTime.now().plusHours(1),1,"Isabella",
+            "Jones",ceoRole,Gender.FEMALE);
+    public static User user14 = new User(2,LocalDateTime.now(),1,LocalDateTime.now().plusHours(1),1,"Richard",
+            "Byrne",ceoRole,Gender.MALE);
+    public static User user15 = new User(1,LocalDateTime.now(),1,LocalDateTime.now().plusHours(1),1,"Joseph",
+            "Evans",cfoRole,Gender.MALE);
+    public static User user16 = new User(1,LocalDateTime.now(),1,LocalDateTime.now().plusHours(1),1,"Barbara",
+            "Martin",cmoRole,Gender.FEMALE);
 
 
 
@@ -75,12 +97,25 @@ public class DataGenerator {
     }
 
     public static List<User> getUsers() {
-        List<User> users = new ArrayList<>();
+
+        List<User> users = new ArrayList<>(50);
+
         users.add(user1);
         users.add(user2);
         users.add(user3);
         users.add(user4);
         users.add(user5);
+        users.add(user6);
+        users.add(user7);
+        users.add(user8);
+        users.add(user9);
+        users.add(user10);
+        users.add(user11);
+        users.add(user12);
+        users.add(user13);
+        users.add(user14);
+        users.add(user15);
+        users.add(user16);
         return users;
     }
 
